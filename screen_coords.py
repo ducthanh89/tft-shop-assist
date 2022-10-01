a5fafb52923b58255689e4ac764351726aedce46
ducthanh89 <ducthanh89@gmail.com>
2022-10-01 23:36:39 +0700

    sửa tọa độ thành game window edit champion list

--- screen_coords.py
+++ screen_coords.py
@@ -2,20 +2,21 @@
 Contains static screen coordinates the bot uses
 Screen coords for 1920x1080 screens
 (x, y, x+w, y+h) for Vec4 locations, (x, y) for Vec2 locations
+Đã đổi tọa độ thành 1920x1040
 """
 
 from vec4 import Vec4, GameWindow
 from vec2 import Vec2
 
-SHOP_POS = Vec4(GameWindow(481, 1039, 1476, 1070))
+SHOP_POS = Vec4(GameWindow(485, 1030, 1470, 1065))
 
-CHAMP_NAME_POS = [Vec4(GameWindow(3, 5, 120, 24), use_screen_offset=False),
-                  Vec4(GameWindow(204, 5, 320, 24), use_screen_offset=False),
-                  Vec4(GameWindow(407, 5, 522, 24), use_screen_offset=False),
-                  Vec4(GameWindow(608, 5, 712, 24), use_screen_offset=False),
-                  Vec4(GameWindow(808, 5, 912, 24), use_screen_offset=False)]
+CHAMP_NAME_POS = [Vec4(GameWindow(13, 5, 120, 26), use_screen_offset=False),
+                  Vec4(GameWindow(204, 5, 320, 26), use_screen_offset=False),
+                  Vec4(GameWindow(403, 5, 520, 26), use_screen_offset=False),
+                  Vec4(GameWindow(593, 5, 700, 26), use_screen_offset=False),
+                  Vec4(GameWindow(778, 5, 882, 26), use_screen_offset=False)]
 
-GOLD_POS = Vec4(GameWindow(870, 883, 920, 909))
+GOLD_POS = Vec4(GameWindow(877, 879, 925, 908))
 
 BUY_LOC = [Vec2(575, 992), Vec2(775, 992), Vec2(
     975, 992), Vec2(1175, 992), Vec2(1375, 992)]
--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@
 TESSERACT_PATH = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 USE_HOT_KEY = True
 HOT_KEY = 'x'
-CHAMPIONS_TO_BUY = ["Aatrox", "Nidalee", "Skarner", "Leona"]
-REROLL = True
+CHAMPIONS_TO_BUY = ["Lux", "Vladimir", "Varus", "Nidalee", "Skarner", "Sylas", "Nomsy", "Aurelion Sol", "Lillia", "Taliyah", "Zoe"]
+REROLL = False
