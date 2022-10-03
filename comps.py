fbc4af61d013805ea0b038370516054546fcbd34
ducthanh89 <41689671+ducthanh89@users.noreply.github.com>
2022-10-03 13:52:50 +0700

    add auto queue

--- comps.py
+++
@@ -1,5 +0,0 @@
-Astral_Mage = ["Lux", "Vladimir", "Varus", "Nidalee", "Skarner", "Sylas", "Nomsy", "Aurelion Sol", "Lillia",
-               "Taliyah", "Zoe", "Ao Shin", 'Bard', 'Jayce']
-Lagoon_Cannoner = ['Zyra', 'Zac', 'Malphite', 'Taliyah', 'Seraphine', 'Zeri', 'Rakan', 'Pantheon', 'Graves', 'Yasuo', 'Bard']
-
-Dragon = ['Ao Shin', 'Shyvana', 'Terra', 'Idas', 'Shi Oh Yu', 'Daeja']
--- settings.py
+++ settings.py
@@ -6,6 +6,15 @@ USE_HOT_KEY = False
 REROLL = False
 HOT_KEY = 'x'
 
-champ_add = []
-CHAMPIONS_TO_BUY = comps.Astral_Mage #+ comps.Lagoon_Cannoner
+
+Astral_Mage = ["Lux", "Vladimir", "Varus", "Nidalee", "Skarner", "Sylas", "Nomsy", "Aurelion Sol", "Lillia",
+               "Taliyah", "Zoe", "Ao Shin", 'Bard', 'Jayce', 'Zyra', 'Jax']
+
+Lagoon_Cannoner = ['Zyra', 'Zac', 'Malphite', 'Taliyah', 'Seraphine', 'Zeri', 'Rakan', 'Pantheon', 'Graves', 'Yasuo', 'Bard']
+
+Dragon = ['Ao Shin', 'Shyvana', 'Terra', 'Idas', 'Shi Oh Yu', 'Daeja']
+
+champ_add = ["Lux", "Sylas", "Aurelion Sol"]
+
+CHAMPIONS_TO_BUY = Astral_Mage + Lagoon_Cannoner
 
