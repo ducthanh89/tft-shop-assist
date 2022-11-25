44b347a80d1e9936242a2c32d61d8e6bd781d4c5
ducthanh89 <41689671+ducthanh89@users.noreply.github.com>
2022-11-25 07:45:41 +0700

    fix bug

--- settings.py
+++ settings.py
@@ -4,20 +4,23 @@ import comps
 queueId = 1100
 LEAGUE_CLIENT_PATH = r'C:\Garena\Games\32787\LeagueClient' # Replace None with your path. Ex: 'C:\\Riot Games\\League of Legends'
 TESSERACT_PATH = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-USE_HOT_KEY = True
+USE_HOT_KEY = False
 REROLL = USE_HOT_KEY
 HOT_KEY = 'x'
+
 
 Mirage_Guide = ['Sejuani', 'Twitch', 'Nunu & Willump', 'Zippy', 'Jayce', 'Daeja', 'Bard']
 Lagoon_Mage = ['Malphite', 'Taliyah', 'Seraphine', 'Sylas', 'Nilah', 'Sohm', 'Bard', 'Zoe']
 Astral_Mage = ["Lux", "Vladimir", "Varus", "Nidalee", "Skarner", "Sylas", "Nomsy", "Aurelion Sol", "Lillia",
                "Taliyah", "Zoe", "Ao Shin", 'Bard', 'Jayce']
-Lagoon_Cannoner = ['Taliyah','Zyra', 'Zac', 'Seraphine', 'Zeri', 'Rakan', 'Pantheon', 'Graves', 'Yasuo', 'Bard']
-Dragon = ['Ao Shin', 'Shyvana', 'Terra', 'Idas', 'Shi Oh Yu', 'Daeja', "Aurelion Sol"]
+Lagoon_Cannoner = ['Zyra', 'Zac', 'Seraphine', 'Zeri', 'Rakan', 'Pantheon', 'Graves', 'Yasuo', 'Bard']
+Dragon = ['Ao Shin', 'Shyvana', 'Terra', 'Idas', 'Shi Oh Yu']
 Dragonmancer = ['Sett','Karma', 'Kaisa', 'Lee Sin', 'Nunu & Willump', 'Volibear', 'Hecarim', 'Yasuo', 'Ao Shin']
 Ragewing_Guild = ['Sejuani', 'Twitch', 'Rakan', 'Jayce', 'Xayah', 'Hecarim', 'Shyvana', 'Bard', 'Zippy']
-champ_add = ['Lux',"Varus",'Sylas']
+Darkflights = ['Aphelios', 'Rell', 'Rengar', 'Swain']
 
 
-CHAMPIONS_TO_BUY = Lagoon_Mage
+champ_add = ['Rakan','Bard']
+
+CHAMPIONS_TO_BUY = Dragon+champ_add
 
